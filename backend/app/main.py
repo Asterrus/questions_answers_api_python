bc4e1fb78885cedbc781f83bde7a1fb22a9c1fd8
roman <gastursolar@gmail.com>
2025-11-14 02:29:28 +0500

    add representation layer, refactoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
 import structlog
 from fastapi import FastAPI
 
-from app.api.main import api_router
 from app.config.logging import setup_logging
+from app.representation.api.rest.v1.routes.answers import router as answers_router
+from app.representation.api.rest.v1.routes.questions import router as questions_router
 
 setup_logging()
 logger = structlog.get_logger(__name__)
 
 
 app = FastAPI()
-app.include_router(api_router)
+app.include_router(answers_router)
+app.include_router(questions_router)
 logger.info("Application started")
